279b6f5ec5ea8b2a7e0372e06a6d82b99d4b6de0
bonuszaa125 <bonuszaa125@gmail.com>
2024-09-22 21:31:22 +0700

    Send_More_Code

--- app.js
+++ app.js
@@ -1,2 +1,65 @@
 const music = new Audio('audio/Rain_Wedding.mp3');
 // music.play();
+
+// Create Array
+const songs = [
+    {
+        id: '1',
+        songName: `เหมือนวิวานห์ (Rain wedding) <br>
+                    <div class="subtitle">Jeff Satur</div>`,
+        poster: "img/Poster/Rain Wedding(Jeff).jpg"
+    },
+    {
+        id: '2',
+        songName: `ซ่อน(ไม่)หา Ghost <br>
+                    <div class="subtitle">Jeff Satur</div>`,
+        poster: "img/Poster/Ghost(Jefft).png"
+    },
+    {
+        id: '3',
+        songName: `Subliminal <br>
+                    <div class="subtitle">BamBam</div>`,
+        poster: "img/Poster/Subliminal(BamBam).jpg"
+    },
+    {
+        id: '4',
+        songName: `Feature <br>
+                    <div class="subtitle">Sabrina Carpenter</div>`,
+        poster: "img/Poster/Feather.jpg"
+    },
+    {
+        id: '5',
+        songName: `Henny(for hennessy) <br>
+                    <div class="subtitle">Jackson Wang</div>`,
+        poster: "img/Poster/Henny(Jackson_W).jpeg"
+    },
+    {
+        id: '6',
+        songName: `ซ่อน(ไม่)หา Ghost <br>
+                    <div class="subtitle">Jeff Satur</div>`,
+        poster: "img/Poster/Ghost(Jefft).png"
+    },
+    {
+        id: '7',
+        songName: `ซ่อน(ไม่)หา Ghost <br>
+                    <div class="subtitle">Jeff Satur</div>`,
+        poster: "img/Poster/Ghost(Jefft).png"
+    },
+    // Secone
+    {
+        id: '8',
+        songName: `เหมือนวิวานห์ (Rain wedding) <br>
+                    <div class="subtitle">Jeff Statur</div>`,
+        poster: "img/Poster/Ghost(Jefft).png"
+    },
+
+
+    // Max to 21
+];
+
+Array.from(document.getElementsByClassName('songItem')).forEach((element, i) => {
+    // อัพเดทรูปภาพตาม array
+    element.getElementsByTagName('img')[0].src = songs[i].poster;
+    // อัพเดทชื่อเพลงและศิลปิน
+    element.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
+});
